41b32334da063d2dd7522cb27631b84861a22d4a
perlmeis <perlmeis>
2004-05-19 02:52:31 +0000

    0.03

--- lib/RRDTool/OO.pm
+++ lib/RRDTool/OO.pm
@@ -7,7 +7,7 @@ use Carp;
 use RRDs;
 use Log::Log4perl qw(:easy);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
    # Define the mandatory and optional parameters for every method.
 our $OPTIONS = {
